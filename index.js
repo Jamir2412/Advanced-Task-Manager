292de1fafa1a7044cb21a6c263d248d1a3ca2143
Jamir Ahmed <jamir.ahmed.ghtwtrlkdn@gmail.com>
2026-02-17 02:10:38 +0530

    Removed Date input

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let tasks= [];
 const form= document.getElementById("task-form");
 const task_input= document.getElementById("task-title");
-const task_date= document.getElementById("task-date");
 const task_priority= document.getElementById("task-priority");
 const task_category= document.getElementById("task-category");
 const task_list= document.getElementById("task-list");
 form.addEventListener("submit", function(e){
     e.preventDefault();
     const title= task_input.value.trim();
-    const dueDate= task_date.value;
     const priority= task_priority.value;
     const category= task_category.value;
 
@@ -17,9 +15,8 @@ form.addEventListener("submit", function(e){
         return;
     }
     const task= {
-        id: Date.now().toString(),
+        time: new Date(),
         title,
-        dueDate,
         priority,
         category
     }
@@ -29,10 +26,17 @@ form.addEventListener("submit", function(e){
 });
 function renderTask(task){
     const li= document.createElement("li");
+    const dd= task.time.getDate();
+    const mm= task.time.getMonth() + 1;
+    const yy= task.time.getFullYear();
+    const date= dd +'/' + mm + '/' + yy;
     li.className= "task-item";
-    li.innerHTML=`<input type="checkbox" /> <span class="task-title">${task.title}</span>
-     <span class="task-category">${task.category}</span> 
-     <span class="task-priority ${task.priority.toLowerCase()}"> ${task.priority} </span> 
-     <button class="edit-btn">✏</button> <button class="delete-btn">🗑</button>`;
+    li.innerHTML=`<div class="title_and_marker"><input type="checkbox" /> <span class="task-title">${task.title}</span></div>
+     <div class="task_info">
+     <span class="task-date task_sub_info">${date}</span> 
+     <span class="task-category task_sub_info">${task.category}</span> 
+     <span class="task-priority task_sub_info ${task.priority.toLowerCase()}"> ${task.priority} </span> 
+     <button class="edit-btn task_sub_info">✏</button> <button class="delete-btn">🗑</button>
+     </div>`;
      task_list.appendChild(li);
 }
